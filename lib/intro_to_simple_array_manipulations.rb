1e46230e3464760854b3d0b147533930669e629a
Emma Fewer <emmafewer20@gmail.com>
2020-10-17 16:59:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array1, array2)
+  array1.insert(array2)
+end
+
+def using_uniq(array)
+end
+
+def using_flatten(array)
+end
+
+def using_delete(array1, array2)
+end
+
+def using_delete_at(array, integer)
+end
